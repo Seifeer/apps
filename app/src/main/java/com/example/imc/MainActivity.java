f4c0860d8aa569afa251a037de4235fe740676d8
Seifeer <xxx@gmail.com>
2019-03-27 17:14:08 -0300

    testando

--- app/src/main/java/com/example/imc/MainActivity.java
+++ app/src/main/java/com/example/imc/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     private float calculo_imc;
     private EditText altura;
     private EditText peso;
+    private TextView textRes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,16 +21,29 @@ public class MainActivity extends AppCompatActivity {
 
         altura=findViewById(R.id.tipo_altura);
         peso=findViewById(R.id.tipo_peso);
-
-        CalcularImc();
-
+        textRes=findViewById(R.id.txt_resultado);
+        btn_imc=findViewById(R.id.tipo_calculo);
     }
 
-    public void CalcularImc(){
+    public void calcular_imc(View view) {
+        if (b)
+
         float imc_altura= Float.parseFloat(altura.getText().toString());
         float imc_peso= Float.parseFloat(peso.getText().toString());
+        float calculo_imc= imc_peso/(imc_altura*imc_altura);
+        String classif="";
 
-        calculo_imc= imc_peso/(imc_altura*imc_altura);
-        Toast.makeText(this, "IMC: "+calculo_imc+"\n"+"Teste", Toast.LENGTH_SHORT).show();
+        if (calculo_imc<18){
+            classif="ABAIXO DO PESO";
+        }else if(calculo_imc>30){
+            classif="OBESIDADE";
+        }else {
+            classif = "Normal";
+        }
+        String result=textRes.setText("Altura: %.2f\nPeso: %.1f\nIMC: %.1f\nSit.:);
+
+        result= String.format(result, altura, peso, calculo_imc);
+        textRes.setVisibility(View.VISIBLE);
+        //Toast.makeText(this, "IMC: "+calculo_imc+"\n"+"Teste", Toast.LENGTH_SHORT).show();
     }
 }
